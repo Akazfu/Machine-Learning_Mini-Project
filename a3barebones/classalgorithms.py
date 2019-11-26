1c717b8c801d03b79c8a2b7aa7ef1d08cd3588c8
LeGrand4K <akazfu@gmail.com>
2019-11-26 10:11:06 -0700

    sub

--- a3barebones/classalgorithms.py
+++ a3barebones/classalgorithms.py
@@ -18,6 +18,7 @@ class Classifier:
     def predict(self, Xtest):
         probs = np.random.rand(Xtest.shape[0])
         ytest = utils.threshold_probs(probs)
+        # ytest = ytest.flatten()
         return ytest
 
 # Susy: ~27 error
@@ -56,19 +57,76 @@ class NaiveBayes(Classifier):
         # If usecolumnones is False, it ignores this last feature
         self.params = utils.update_dictionary_items({'usecolumnones': False}, parameters)
 
-
     def learn(self, Xtrain, ytrain):
         # obtain number of classes
         if ytrain.shape[1] == 1:
             self.numclasses = 2
+            self.numsamples = ytrain.shape[0]
+            # print(self.numsamples)
+            # If usecolumnones is False, it ignores this last feature
+            if self.params['usecolumnones'] == False:
+                Xtrain = Xtrain[:, :8].copy()
+            self.numfeatures = Xtrain.shape[1]
+            # print(self.numfeatures)
+
+            count = np.zeros((2,), dtype=int)
+            sample_class = [[], []]
+
+            # Compute prior P(c) = 0 and P(c) = 1 from the dataset if it is there
+            for i in range (self.numsamples):
+                if ytrain[i] == 1:
+                    count[1] += 1
+                else:
+                    count[0] += 1
+            self.p_0 = count[0]/self.numsamples
+            self.p_1 = count[1]/self.numsamples
+            self.mean = np.zeros((self.numclasses, self.numfeatures))
+            self.std = np.zeros((self.numclasses, self.numfeatures))
+
+            for i in range (self.numsamples):
+                if ytrain[i] == 1:
+                    sample_class[1].append(Xtrain[i])
+                elif ytrain[i] == 0:
+                    sample_class[0].append(Xtrain[i])
+            
+            for c in range(self.numclasses):
+                for j in range (self.numfeatures):
+                    mean = []
+                    for i in range(count[c]):
+                        mean.append(sample_class[c][i][j])
+                    self.mean[c][j] = np.mean(mean)
+                    self.std[c][j] = np.std(mean)                
         else:
             raise Exception('Can only handle binary classification')
 
     def predict(self, Xtest):
-        numsamples = Xtest.shape[0]
+        numtest = Xtest.shape[0]
         predictions = []
+        pred = np.ones((self.numclasses, numtest))
 
-        return np.reshape(predictions, [numsamples, 1])
+        for i in range (numtest):
+            # print('self.numfeatures is '+str(self.numfeatures))
+            for j in range (self.numfeatures):
+                if self.std[0][j] == 0:
+                    pred[0][i] = pred[0][i] * 1
+                else:
+                    pred[0][i] = pred[0][i] * (1.0/np.sqrt(2*np.pi*(self.std[0][j]**2))) * np.exp(-1.0*np.square(Xtest[i][j]-self.mean[0][j])/(2*(self.std[0][j]**2)))
+                
+                if self.std[1][j] == 0:
+                    pred[1][i] = pred[1][i] * 1
+                else:
+                    pred[1][i] = pred[1][i] * (1.0/np.sqrt(2*np.pi*(self.std[1][j]**2))) * np.exp(-1.0*np.square(Xtest[i][j]-self.mean[1][j])/(2*(self.std[1][j]**2)))
+
+            pred[0][i] = pred[0][i] * self.p_0
+            pred[1][i] = pred[1][i] * self.p_1
+       
+        for i in range (numtest):
+            if pred[1][i] >= pred[0][i]:
+                predictions.insert(i, 1)
+            else:
+                predictions.insert(i, 0)
+
+        return np.reshape(predictions, [numtest, 1])
 
 # Susy: ~23 error
 class LogisticReg(Classifier):
@@ -77,10 +135,28 @@ class LogisticReg(Classifier):
         self.weights = None
 
     def learn(self, X, y):
-        pass
+        self.weights = np.zeros(X.shape[1],)
+        epochs = self.params["epochs"]
+        stepSize = self.params["stepsize"]
+        for j in range(epochs):
+            for i in range(X.shape[0]):
+                sigmoid = utils.sigmoid(X[i] @ self.weights)
+                # print (sigmoid)
+                self.weights -= stepSize * (sigmoid-y[i]) * X[i]
 
     def predict(self, Xtest):
-        pass
+        predictions = []
+        numtest = Xtest.shape[0]
+
+        for i in range(numtest):
+            prob = utils.sigmoid(np.dot(Xtest, self.weights))
+            if  prob[i] >=0.5:
+                predictions.insert(i, 1)
+            else:
+                predictions.insert(i, 0)
+      
+        return np.reshape(predictions, [numtest, 1])
+
 
 # Susy: ~23 error (4 hidden units)
 class NeuralNet(Classifier):
@@ -99,14 +175,48 @@ class NeuralNet(Classifier):
             # For now, only allowing sigmoid transfer
             raise Exception('NeuralNet -> can only handle sigmoid transfer, must set option transfer to string sigmoid')
 
-        self.wi = None
-        self.wo = None
-
+        # self.wi = None
+        # self.wo = None
+    
     def learn(self, Xtrain, ytrain):
-        pass
+        numsamples = Xtrain.shape[0]
+        numfeatures = Xtrain.shape[1]
+        epochs = self.params["epochs"]
+
+        self.wi = np.random.randn(self.params['nh'], numfeatures)
+        self.wo = np.random.randn(1, self.params['nh'])
+
+        for epoch in range(epochs):
+            for i in range(numsamples):
+                xtrain = np.reshape(Xtrain[i],(-1,1))
+                # Hidden Layer 1
+                h1 = self.transfer(np.dot(self.wi,xtrain))
+                h1 = np.reshape(h1,(-1,1))
+                predicted = self.transfer(np.dot(self.wo,h1))
+                
+                # Calculate the gradients
+                delta = (-ytrain[i]*(1-predicted)) + (1-ytrain[i]) * predicted
+                grad_wo = np.dot(delta,h1.T)
+                grad_int = (self.wo*h1.T*(1-h1.T)).T
+                xtrain = np.reshape(Xtrain[i].T,(-1,1))
+                grad_wi = delta*np.dot(grad_int,xtrain.T)
+
+                # Call update function to update the weights
+                self.update(grad_wi, grad_wo)
 
     def predict(self,Xtest):
-        pass
+        predictions = []
+        numtest = Xtest.shape[0]
+        
+        for i in range(0,numtest):
+            # Retrieve classes label from (ah, ao)
+            ao = self.evaluate(Xtest[i])[1]
+            if ao > 0.5:
+               predictions.insert(i, 1)
+            else:
+               predictions.insert(i, 0)
+               
+        return np.reshape(predictions, [numtest, 1])
 
     def evaluate(self, inputs):
         # hidden activations
@@ -121,7 +231,8 @@ class NeuralNet(Classifier):
         )
 
     def update(self, inputs, outputs):
-        pass
+        self.wi= self.wi - self.params['stepsize'] * inputs
+        self.wo=  self.wo - self.params['stepsize'] * outputs
 
 # Note: high variance in errors! Make sure to run multiple times
 # Susy: ~28 error (40 centers)
@@ -132,10 +243,199 @@ class KernelLogisticRegression(LogisticReg):
             'epochs': 100,
             'centers': 10,
         }, parameters)
-        self.weights = None
+        # self.weights = None
 
     def learn(self, X, y):
-        pass
+        centers = self.params['centers']
+        epochs = self.params["epochs"]
+        stepSize = self.params["stepsize"]
+        self.centerlist = []
+
+        for c in range(centers):
+            index = np.random.choice(X.shape[0])
+            self.centerlist.append(X[index])
+        
+        #initialize kernel as n by k zero matrix 
+        kernel = np.zeros((X.shape[0], self.params['centers']))
+        for i in range (kernel.shape[0]):
+            for j in range(kernel.shape[1]):
+                kernel[i][j] = np.dot(X[i],self.centerlist[j])
+
+        self.weights = np.zeros(kernel.shape[1],)
+        for epoch in range(epochs):
+            for i in range(kernel.shape[0]):
+                sigmoid = utils.sigmoid(kernel[i] @ self.weights)
+                # print(sigmoid)
+                self.weights -= stepSize * (sigmoid-y[i]) * kernel[i]
 
     def predict(self, Xtest):
-        pass
+        predictions = []
+        numtest = Xtest.shape[0]
+
+        Ktest = np.zeros((numtest, self.params['centers']))
+        for n in range(numtest):
+            for m in range(len(self.centerlist)):
+                # print(Ktest[n])
+                # print(Xtest[n])
+                # print(self.centerlist[m])
+                Ktest[n][m] = np.dot(Xtest[n], self.centerlist[m])
+
+        newtest = np.dot(Ktest, self.weights)
+        for i in range(newtest.shape[0]):
+            prob = utils.sigmoid(newtest)
+            if  prob[i] >=0.5:
+                predictions.insert(i, 1)
+            else:
+                predictions.insert(i, 0)
+      
+        return np.reshape(predictions, [numtest, 1])
+
+
+# Question 2 b) To test the KernelLogisticRegressionHamming ==> Uncomment the censes dataset line at script_classify.py
+class KernelLogisticRegressionHamming(LogisticReg):
+    def __init__(self, parameters = {}):
+        self.params = utils.update_dictionary_items({
+            'stepsize': 0.01,
+            'epochs': 100,
+            'centers': 10,
+        }, parameters)
+        # self.weights = None
+
+    # A simple Implementation for compute the hamming distance
+    def hamming(self, x, c):
+        distance = 0
+        for i in range(len(x)):
+            # if two arrays are not identical distance +1
+            if x[i] != c[i]:
+                distance += 1
+        return distance
+
+    def learn(self, X, y):
+        centers = self.params['centers']
+        epochs = self.params["epochs"]
+        stepSize = self.params["stepsize"]
+        self.centerlist = []
+
+        for c in range(centers):
+            index = np.random.choice(X.shape[0])
+            self.centerlist.append(X[index])
+        
+        #initialize kernel as n by k zero matrix 
+        kernel = np.zeros((X.shape[0], self.params['centers']))
+        for i in range (kernel.shape[0]):
+            for j in range(kernel.shape[1]):
+                kernel[i][j] = self.hamming(X[i],self.centerlist[j])
+
+        self.weights = np.zeros(kernel.shape[1],)
+        for epoch in range(epochs):
+            for i in range(kernel.shape[0]):
+                sigmoid = utils.sigmoid(kernel[i] @ self.weights)
+                # print(sigmoid)
+                self.weights -= stepSize * (sigmoid-y[i]) * kernel[i]
+
+    def predict(self, Xtest):
+        predictions = []
+        numtest = Xtest.shape[0]
+
+        Ktest = np.zeros((numtest, self.params['centers']))
+        for n in range(numtest):
+            for m in range(len(self.centerlist)):
+                # print(Ktest[n])
+                # print(Xtest[n])
+                # print(self.centerlist[m])
+                Ktest[n][m] = self.hamming(Xtest[n], self.centerlist[m])
+
+        newtest = np.dot(Ktest, self.weights)
+        for i in range(newtest.shape[0]):
+            prob = utils.sigmoid(newtest)
+            if  prob[i] >=0.5:
+                predictions.insert(i, 1)
+            else:
+                predictions.insert(i, 0)
+      
+        return np.reshape(predictions, [numtest, 1])
+
+# Bonus a) neural network of two hidden layers
+class NeuralNet2Hidden(Classifier):
+    def __init__(self, parameters={}):
+        self.params = utils.update_dictionary_items({
+            'nh': 4,
+            'transfer': 'sigmoid',
+            'stepsize': 0.01,
+            'epochs': 10,
+        }, parameters)
+
+        if self.params['transfer'] is 'sigmoid':
+            self.transfer = utils.sigmoid
+            self.dtransfer = utils.dsigmoid
+        else:
+            # For now, only allowing sigmoid transfer
+            raise Exception('NeuralNet -> can only handle sigmoid transfer, must set option transfer to string sigmoid')
+
+        # self.wi = None
+        # self.wo = None
+    
+    def learn(self, Xtrain, ytrain):
+        numsamples = Xtrain.shape[0]
+        numfeatures = Xtrain.shape[1]
+        epochs = self.params["epochs"]
+
+        self.wi = np.random.randn(self.params['nh'], numfeatures)
+        self.wo1 = np.random.randn(2, self.params['nh'])
+        self.wo2 = np.random.randn(1, self.params['nh'])
+
+        for epoch in range(epochs):
+            for i in range(numsamples):
+                xtrain = np.reshape(Xtrain[i],(-1,1))
+                # Hidden Layer 1
+                h1 = self.transfer(np.dot(self.wi,xtrain))
+                h1 = np.reshape(h1,(-1,1))
+                hout = self.transfer(np.dot(self.wo1,h1))
+                
+                # Hidden Layer 2
+                h2 = self.transfer(np.dot(self.wo1,hout))
+                h2 = np.reshape(h2,(-1,1))
+                predicted = self.transfer(np.dot(self.wo2,h2))
+
+                # Calculate the gradients
+                delta = (-ytrain[i]*(1-hout)) + (1-ytrain[i]) * hout
+                grad_wo1 = np.dot(delta,h1.T)
+                delta2 = (-hout[i]*(1-predicted)) + (1-hout[i]) * predicted
+                grad_wo2 = np.dot(delta2,h2.T)
+                grad_int = (self.wo1*h1.T*(1-h1.T)).T
+                xtrain = np.reshape(Xtrain[i].T,(-1,1))
+                grad_wi = delta*np.dot(grad_int,xtrain.T)
+
+                # Call update function to update the weights
+                self.update(grad_wi, grad_wo1, grad_wo2)
+
+    def predict(self,Xtest):
+        predictions = []
+        numtest = Xtest.shape[0]
+        
+        for i in range(0,numtest):
+            # Retrieve classes label from (ah, ao)
+            ao = self.evaluate(Xtest[i])[1]
+            if ao > 0.5:
+               predictions.insert(i, 1)
+            else:
+               predictions.insert(i, 0)
+               
+        return np.reshape(predictions, [numtest, 1])
+
+    def evaluate(self, inputs):
+        # hidden activations
+        ah = self.transfer(np.dot(self.wi,inputs.T))
+
+        # output activations
+        ao = self.transfer(np.dot(self.wo,ah)).T
+
+        return (
+            ah, # shape: [nh, samples]
+            ao, # shape: [classes, nh]
+        )
+
+    def update(self, i, o1, o2):
+        self.wi = self.wi - self.params['stepsize'] * i
+        self.wo1 =  self.wo1 - self.params['stepsize'] * o1
+        self.wo2 =  self.wo2 - self.params['stepsize'] * o2
--- a3barebones/script_classify.py
+++ a3barebones/script_classify.py
@@ -26,16 +26,36 @@ Check utilities.py for example usage.
 """
 def cross_validate(K, X, Y, Algorithm, parameters):
     all_errors = np.zeros((len(parameters), K))
+
     for k in range(K):
+        # Split the dataset to train set and test set according to number of folds k
+        X_n = np.split(X, K)
+        Y_n = np.split(Y, K)
+
+        Xtest = X_n.pop(k)
+        Ytest = Y_n.pop(k)
+        Xtrain = np.concatenate(X_n)
+        Ytrain = np.concatenate(Y_n)
+
         for i, params in enumerate(parameters):
-            pass
+            learner = Algorithm (params)
+            # Train model
+            learner.learn(Xtrain, Ytrain)
+            # Test model
+            predictions = learner.predict(Xtest)
+            error = geterror(Ytest, predictions)
+            all_errors[i, k] = error
 
     avg_errors = np.mean(all_errors, axis=1)
+    best_error = np.inf
+
     for i, params in enumerate(parameters):
         print('Cross validate parameters:', params)
         print('average error:', avg_errors[i])
+        if avg_errors[i] < best_error:
+            best_error = avg_errors[i]
+            best_parameters = parameters[i]
 
-    best_parameters = parameters[0]
     return best_parameters
 
 if __name__ == '__main__':
@@ -47,26 +67,31 @@ if __name__ == '__main__':
                         help='Specify the train set size')
     parser.add_argument('--testsize', type=int, default=5000,
                         help='Specify the test set size')
-    parser.add_argument('--numruns', type=int, default=10,
+    parser.add_argument('--numruns', type=int, default=1,
                         help='Specify the number of runs')
     parser.add_argument('--dataset', type=str, default="susy",
                         help='Specify the name of the dataset')
+    # Added an extra argument for number of folds for debugging
+    parser.add_argument('--numfolds', type=int, default=2,
+                        help='Specify the number of folds')
 
     args = parser.parse_args()
     trainsize = args.trainsize
     testsize = args.testsize
     numruns = args.numruns
     dataset = args.dataset
-
-
+    """ Uncomment the nextline if want to test KernelLogisticRegressionHamming on censes dataset """
+    # dataset = 'census'
+    numfolds = args.numfolds
 
     classalgs = {
-        'Random': algs.Classifier,
+        # 'Random': algs.Classifier,
         'Naive Bayes': algs.NaiveBayes,
-        'Linear Regression': algs.LinearRegressionClass,
+        # 'Linear Regression': algs.LinearRegressionClass,
         'Logistic Regression': algs.LogisticReg,
         'Neural Network': algs.NeuralNet,
         'Kernel Logistic Regression': algs.KernelLogisticRegression,
+        # 'KernelLogisticRegressionHamming': algs.KernelLogisticRegressionHamming,
     }
     numalgs = len(classalgs)
 
@@ -83,6 +108,7 @@ if __name__ == '__main__':
         'Logistic Regression': [
             { 'stepsize': 0.001 },
             { 'stepsize': 0.01 },
+            { 'stepsize': 0.1 },
         ],
         'Neural Network': [
             { 'epochs': 100, 'nh': 4 },
@@ -95,6 +121,12 @@ if __name__ == '__main__':
             { 'centers': 20, 'stepsize': 0.01 },
             { 'centers': 40, 'stepsize': 0.01 },
             { 'centers': 80, 'stepsize': 0.01 },
+        ],
+        'KernelLogisticRegressionHamming': [
+            { 'centers': 10, 'stepsize': 0.01 },
+            { 'centers': 20, 'stepsize': 0.01 },
+            { 'centers': 40, 'stepsize': 0.01 },
+            { 'centers': 80, 'stepsize': 0.01 },
         ]
     }
 
@@ -102,7 +134,7 @@ if __name__ == '__main__':
     errors = {}
     for learnername in classalgs:
         errors[learnername] = np.zeros(numruns)
-
+        
     for r in range(numruns):
         if dataset == "susy":
             trainset, testset = dtl.load_susy(trainsize, testsize)
@@ -116,21 +148,33 @@ if __name__ == '__main__':
         Ytrain = trainset[1]
         # cast the Y vector as a matrix
         Ytrain = np.reshape(Ytrain, [len(Ytrain), 1])
-
         Xtest = testset[0]
         Ytest = testset[1]
         # cast the Y vector as a matrix
         Ytest = np.reshape(Ytest, [len(Ytest), 1])
 
         best_parameters = {}
+
         for learnername, Learner in classalgs.items():
             params = parameters.get(learnername, [ None ])
-            best_parameters[learnername] = cross_validate(10, Xtrain, Ytrain, Learner, params)
+            print('\n\nRunning learner = ' + learnername + '\tRun = ' + str(r))
+            best_parameters[learnername] = cross_validate(numfolds, Xtrain, Ytrain, Learner, params)
 
+        print('\n')
         for learnername, Learner in classalgs.items():
             params = best_parameters[learnername]
+            print('Best Parameter for ' + learnername + 'is '+ str(params))
             learner = Learner(params)
+            # Train model
+            learner.learn(Xtrain, Ytrain)
+            # Test model
+            pred = learner.predict(Xtest)
+            error = geterror(Ytest, pred)
+            errors[learnername][r] = error
 
+    print('\n' * 2)
     for learnername in classalgs:
         aveerror = np.mean(errors[learnername])
+        stderror = np.std(errors[learnername]) / np.sqrt(numruns)
         print('Average error for ' + learnername + ': ' + str(aveerror))
+        print('Standard error for ' + learnername + ': ' + str(stderror))

10466be999746acf84f25eb6f7980b4a43bd4df1
jason <jiazhen2@ualberta.ca>
2019-11-26 20:35:13 -0700

    update dataset

---
+++ dataset/converttocsv.py
@@ -0,0 +1,19 @@
+import csv
+
+with open('adult.data') as input_file:
+   lines = input_file.readlines()
+   newLines = []
+   for line in lines:
+      newLine = line.strip().split(",")
+      if newLine[-1] == " <=50K":
+          newLine[-1] = 0;
+      elif newLine[-1] == " >50K":
+          newLine[-1] = 1;
+      #print(newLine[-1])
+
+
+      newLines.append( newLine )
+
+with open('adult.csv', 'wb') as test_file:
+   file_writer = csv.writer(test_file)
+   file_writer.writerows( newLines )

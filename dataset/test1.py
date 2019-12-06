a5eb7172aa47d38ffc076691bbd5699cc6e0e582
jason <jiazhen2@ualberta.ca>
2019-12-05 22:02:34 -0700

    Update test1.py

--- dataset/test1.py
+++ dataset/test1.py
@@ -3,10 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
 from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+from scipy import stats
 
 data = pd.read_csv("adult.csv",header =0)
 data = data.dropna()
+
+########Optimize feature#############
 
 #print(data["y"].value_counts())
 #print(data)
@@ -19,8 +30,12 @@ data = data.dropna()
 #plt.savefig('capitalloss')
 #plt.show()
 #
+
+#####classify the category feature#################
 tem = list(data.columns)
 tem.remove('y')
+tem.remove('capitalgain')
+tem.remove('capitalloss')
 cat_vars = tem
 for var in cat_vars:
     cat_list='var'+'_'+var
@@ -34,18 +49,219 @@ to_keep=[i for i in data_vars if i not in cat_vars]
 data_final=data[to_keep]
 #print(data_final.columns.values)
 
+####################################################################
+
+#over sampling data using smote
+
+####################################################################
+
 X = data_final.loc[:, data_final.columns != 'y']
 y = data_final.loc[:, data_final.columns == 'y']
-from imblearn.over_sampling import SMOTE
-os = SMOTE(random_state=0)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-columns = X_train.columns
-os_data_X,os_data_y=os.fit_sample(X_train, y_train)
-os_data_X = pd.DataFrame(data=os_data_X,columns=columns )
-os_data_y= pd.DataFrame(data=os_data_y,columns=['y'])
-# we can Check the numbers of our data
-print("length of oversampled data is ",len(os_data_X))
-print("Number of no subscription in oversampled data",len(os_data_y[os_data_y['y']==0]))
-print("Number of subscription",len(os_data_y[os_data_y['y']==1]))
-print("Proportion of no subscription data in oversampled data is ",len(os_data_y[os_data_y['y']==0])/len(os_data_X))
-print("Proportion of subscription data in oversampled data is ",len(os_data_y[os_data_y['y']==1])/len(os_data_X))
+#from imblearn.over_sampling import SMOTE
+#os = SMOTE(random_state=0)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+#columns = X_train.columns
+#os_data_X,os_data_y=os.fit_sample(X_train, np.ravel(y_train,order = 'C'))
+#os_data_X = pd.DataFrame(data=os_data_X,columns=columns )
+#os_data_y= pd.DataFrame(data=os_data_y,columns=['y'])
+## we can Check the numbers of our data
+#print("length of oversampled data is ",len(os_data_X))
+#print("Number of no subscription in oversampled data",len(os_data_y[os_data_y['y']==0]))
+#print("Number of subscription",len(os_data_y[os_data_y['y']==1]))
+#print("Proportion of no subscription data in oversampled data is ",len(os_data_y[os_data_y['y']==0])/len(os_data_X))
+#print("Proportion of subscription data in oversampled data is ",len(os_data_y[os_data_y['y']==1])/len(os_data_X))
+
+
+####################################################################
+
+# Use Recursive Feature Elimination to choose best feature
+
+####################################################################
+
+
+#data_final_vars=data_final.columns.values.tolist()
+#y=['y']
+#X=[i for i in data_final_vars if i not in y]
+#logreg = LogisticRegression(max_iter=120,solver='lbfgs')
+#rfe = RFE(logreg, 20)
+#rfe = rfe.fit(X_train, y_train.values.ravel())
+#print(rfe.support_)
+#print(rfe.ranking_)
+#
+
+####################################################################
+
+# Cross validation with ROC curve and stats data
+
+####################################################################
+
+#if "SVM":
+cv = StratifiedKFold(n_splits=10)
+classifier = svm.SVC(kernel='linear', probability=True,
+                     random_state=random_state)
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+fig, ax = plt.subplots()
+for i, (train, test) in enumerate(cv.split(X, y)):
+    classifier.fit(X[train], y[train])
+    viz = plot_roc_curve(classifier, X[test], y[test],
+                         name='ROC fold {}'.format(i),
+                         alpha=0.3, lw=1, ax=ax)
+    interp_tpr = interp(mean_fpr, viz.fpr, viz.tpr)
+    interp_tpr[0] = 0.0
+    tprs.append(interp_tpr)
+    aucs.append(viz.roc_auc)
+
+ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+        label='Chance', alpha=.8)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+ax.plot(mean_fpr, mean_tpr, color='b',
+        label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+        lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                label=r'$\pm$ 1 std. dev.')
+
+ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+       title="Receiver operating characteristic example")
+ax.legend(loc="lower right")
+plt.show()
+
+y1_pred = cross_val_predict(classifier, X, np.ravel(y,order = 'C'), cv=10)
+conf_mat = confusion_matrix(y, y1_pred)
+print(conf_mat)
+print(classification_report(y_test, y1_pred))
+
+
+
+#if "Naive bayes"
+cv = StratifiedKFold(n_splits=10)
+classifier = GaussianNB()
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+fig, ax = plt.subplots()
+for i, (train, test) in enumerate(cv.split(X, y)):
+    classifier.fit(X[train], y[train])
+    viz = plot_roc_curve(classifier, X[test], y[test],
+                         name='ROC fold {}'.format(i),
+                         alpha=0.3, lw=1, ax=ax)
+    interp_tpr = interp(mean_fpr, viz.fpr, viz.tpr)
+    interp_tpr[0] = 0.0
+    tprs.append(interp_tpr)
+    aucs.append(viz.roc_auc)
+
+ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+        label='Chance', alpha=.8)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+ax.plot(mean_fpr, mean_tpr, color='b',
+        label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+        lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                label=r'$\pm$ 1 std. dev.')
+
+ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+       title="Receiver operating characteristic example")
+ax.legend(loc="lower right")
+plt.show()
+
+y2_pred = cross_val_predict(classifier, x, y, cv=10)
+conf_mat = confusion_matrix(y, y2_pred)
+print(classification_report(y_test, y2_pred))
+
+#if "logistic regression"
+cv = StratifiedKFold(n_splits=10)
+classifier = LogisticRegression(solver='liblinear')
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+fig, ax = plt.subplots()
+for i, (train, test) in enumerate(cv.split(X, y)):
+    classifier.fit(X[train], y[train])
+    viz = plot_roc_curve(classifier, X[test], y[test],
+                         name='ROC fold {}'.format(i),
+                         alpha=0.3, lw=1, ax=ax)
+    interp_tpr = interp(mean_fpr, viz.fpr, viz.tpr)
+    interp_tpr[0] = 0.0
+    tprs.append(interp_tpr)
+    aucs.append(viz.roc_auc)
+
+ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+        label='Chance', alpha=.8)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+ax.plot(mean_fpr, mean_tpr, color='b',
+        label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+        lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                label=r'$\pm$ 1 std. dev.')
+
+ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+       title="Receiver operating characteristic example")
+ax.legend(loc="lower right")
+plt.show()
+
+
+y3_pred = cross_val_predict(classifier, x, y, cv=10)
+conf_mat = confusion_matrix(y, y3_pred)
+print(classification_report(y_test, y3_pred))
+
+
+####################################################
+
+# Wilcoxon signed rank test
+
+####################################################
+
+difference =[]
+# SVM & Naive Bayes
+for i in range(length(y1_pred)):
+    difference[i] = y1_pred[i]-y2_pred[i]
+    
+w, p = stats.wilcoxon(difference)
+print("SVM & Naive:w:",w,"p:",p)
+
+# SVM & logistic regression
+for i in range(length(y1_pred)):
+    difference[i] = y1_pred[i]-y3_pred[i]
+    
+w, p = stats.wilcoxon(difference)
+print("SVM & Logistic:w:",w,"p:",p)
+
+# Naive Bayes & logistic regression
+for i in range(length(y1_pred)):
+    difference[i] = y1_pred[i]-y3_pred[i]
+    
+w, p = stats.wilcoxon(difference)
+print("Naive Bayes & Logistic:w:",w,"p:",p)
+
+
